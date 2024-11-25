86c0dd58dede2682d1c60d294a6564f92ed229a7
Shalaka Thorat <shalaka.thorat.432@gmail.com>
2024-11-26 00:35:56 +0530

    Fix: Create Poll Not getting Submitted

--- poll_app/app/routes.py
+++ poll_app/app/routes.py
@@ -12,11 +12,8 @@ app = create_app()
 
 @app.route("/")
 def index():
-
-
     if current_user.is_authenticated:
         return redirect(url_for('home'))
-    
     return redirect(url_for('login'))
 
 @login_manager.user_loader
@@ -56,22 +53,18 @@ def signup():
 @app.route('/home')
 @login_required
 def home():
-
-    
     polls = ps.fetch_polls()
     return render_template("custom_polls.html", user=current_user, polls=polls, page="home")
 
 @app.route('/create_poll', methods=['GET', 'POST'])
 @login_required
 def create_poll():
-
     if request.method == 'POST':
         title = request.form['title']
         description = request.form['description']
         poll = ps.create_poll(title=title, description=description, user_id=current_user.id)
         options = [value for key, value in request.form.items() if key.startswith('option')]
         ps.create_options(options=options, poll_id=poll.id)
-        
         return redirect(url_for('my_polls'))
     return render_template('create_poll.html')
 
@@ -85,7 +78,6 @@ def poll(poll_id):
 @app.route("/results/<poll_id>", methods=['GET', 'POST'])
 @login_required
 def results(poll_id):
-
     if request.method == 'POST':
         selected_option_id=request.form['selected_option']
         ps.vote(user_id=current_user.id, poll_id=poll_id, option_id=selected_option_id)
@@ -94,32 +86,26 @@ def results(poll_id):
 @app.route("/home/my_votes")
 @login_required
 def my_votes():
-
     polls = ps.get_polls_voted_by_user(user_id=current_user.id)
     return render_template("custom_polls.html", user=current_user, polls=polls, page="history")
-    
     
 @app.route("/home/my_polls")
 @login_required
 def my_polls():
-
     polls = ps.get_polls_created_by_user(user_id=current_user.id)
     return render_template("my_polls.html", user=current_user, polls=polls, page="poll")
 
 @app.route("/home/my_polls/delete/<poll_id>")
 @login_required
 def delete_poll(poll_id):
-
     ps.delete_poll(user_id=current_user.id, poll_id=poll_id)
     if current_user.is_admin:
         return redirect(url_for('admin_polls'))
     return redirect(url_for('my_polls'))
 
-
 @app.route("/delete_account")
 @login_required
 def delete_account():
-
     auth.delete_user(user_id=current_user.id)
     return redirect(url_for('index'))
 
@@ -133,9 +119,6 @@ def share_poll(poll_id):
     qr.save(buffered, format="PNG")
     qr_img_bytes= base64.b64encode(buffered.getvalue()).decode()
     return render_template('share_poll.html', url=url, qr_img_bytes=qr_img_bytes)
-
-
-
 
 @app.route('/admin')
 @auth.admin_required
